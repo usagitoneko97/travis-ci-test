7f92b96f68ea9a8cd2ac4b89a98daf2a0288d8ac
usagitoneko <hogouxian@gmail.com>
2018-07-18 14:59:47 +0800

    added subtract function

--- add.py
+++ add.py
@@ -1,2 +1,6 @@
 def add(v1, v2):
     return v1 + v2
+
+
+def sub(v1, v2):
+    return v1 - v2
--- test_add.py
+++ test_add.py
@@ -3,3 +3,7 @@ import add
 
 def test_add_1_with_1_expect_2():
     assert add.add(1, 1) == 2
+
+
+def test_sub_2_with_1_expect_1():
+    assert add.sub(2, 1) == 1
